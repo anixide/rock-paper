60fb0fff6452d8ebe5a17d1b0fabb99b3221701c
g0thman <g.othman.anis@gmail.com>
2020-02-18 02:18:26 +0100

    Rock, Paper, Scissors game completed!!

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 // Working on the browser with prompt
-const outcomes = ['Rock', 'Paper', 'Scissors'];
-const computerPlay = outcomes[Math.floor(Math.random() * outcomes.length)];
-var playerPlay = prompt("What is your play?");
 
-function theGame(playerPlay, computerPlay) {
-    if(playerPlay === computerPlay) {
-        return "It's a draw!";
+
+
+var i = 0;
+while ( i < 5) {
+        const outcomes = ['Rock', 'Paper', 'Scissors'];
+        const computerPlay = outcomes[Math.floor(Math.random() * outcomes.length)];
+        var playerPlay = prompt("What is your play?");
+    function playRound(playerPlay, computerPlay) {
+    
+        if(playerPlay === computerPlay) {
+            return "It's a draw!";
+        }
+        else if (playerPlay === 'Rock' && computerPlay === 'Scissors') {
+            return "K.O! Rock smashes Scissors";
+        }
+        else if (playerPlay === 'Scissors' && computerPlay === 'Paper') {
+            return "Cut down!! Scissors cuts Paper";
+        }
+        else if (playerPlay === 'Paper' && computerPlay === 'Rock') {
+            return "Fully covered!! Paper covered Rock";
+        }
+        else {
+            return "Computers run the world!"
+        }
+    
     }
-    else if (playerPlay === 'Rock' && computerPlay === 'Scissors') {
-        return "K.O!";
-    }
-    else if (playerPlay === 'Scissors' && computerPlay === 'Paper') {
-        return "Cut down!!";
-    }
-    else if (playerPlay === 'Paper' && computerPlay === 'Rock') {
-        return "Fully covered!!";
-    }
-    else {
-        return "Computers run the world!"
-    }
+console.log(playRound(playerPlay, computerPlay));
+i++;
+
 }
- 
 
-console.log(theGame(playerPlay, computerPlay));
+
+
